a21fe89205129ddb4d94f100dc0ec11f0e4af3b7
E. Goldenberg <egorgoldenberg@gmail.com>
2026-04-22 18:41:54 +0700

    формулировки

--- services/procurement_reference_registry.py
+++ services/procurement_reference_registry.py
@@ -292,36 +292,41 @@ class ProcurementReferenceRegistry:
         if not name:
             if exact_okpd_match:
                 message = (
-                    f"Код {query_code} найден в таблице '{short_table_title}'.\n"
+                    f"<ins>Обратите внимание<ins> Код {query_code} <ins>Входит в перечень<ins> '{short_table_title}'.\n"
                     f"Эталонное наименование: {reference_name}.\n"
+                    f"<ins>Необходимо учесть требования постановления при проведении закупки.<ins>"
                 )
             else:
                 message = (
-                    f"Код {query_code}\n"
-                    f'Обратите внимание, родительский код {matched_code} найден в перечне "{short_table_title}".\n'
+                    f"<ins>Обратите внимание<ins> на Код {query_code}.\n"
+                    f'Родительский код {matched_code} <ins>Входит в перечень<ins> "{short_table_title}".\n'
                     f"Эталонное наименование: {reference_name}.\n"
                     f"Проверьте соответствует ли ваше наименование '{name}'."
+                    f"<ins>Необходимо учесть требования постановления при проведении закупки.<ins>"
                 )
         else:
             if exact_okpd_match and (exact_name_match or normalized_name_match):
                 message = (
-                    f"Код {query_code} найден. Наименование совпадает с эталонной записью "
-                    f"из таблицы '{short_table_title}'."
+                    f"<ins>Обратите внимание<ins> Код {query_code} <ins>Входит в перечень<ins> '{short_table_title}'.\n"
+                    f"<ins>Необходимо учесть требования постановления при проведении закупки.<ins>"
                 )
             elif exact_okpd_match:
                 message = (
-                    f"Код {query_code} найден\n"
-                    f"в таблице '{short_table_title}',\n"
-                    f"но наименование отличается от эталонного.\n"
-                    f"Эталонное наименование: {reference_name}.\n"
-                    f"Проверьте соответствует ли ваше наименование '{name}'."
+                    f"<ins>Обратите внимание<ins> Код {query_code} <ins>Входит в перечень<ins> \n"
+                    f"'{short_table_title}',\n"
+                    f"<ins>но наименование отличается от эталонного.<ins>\n"
+                    f"<ins>Эталонное наименование:<ins> {reference_name}.\n"
+                    f"Проверьте соответствует ли ваше наименование '{name}'.\n"
+                    f"<ins>Необходимо учесть требования постановления при проведении закупки.<ins>"
+                     
                 )
             else:
                 message = (
                     f"Код {query_code}\n"
-                    f'Обратите внимание, родительский код {matched_code} найден в перечне "{short_table_title}".\n'
+                    f'<ins>Обратите внимание<ins>, родительский код {matched_code} <ins>Входит в перечень<ins> "{short_table_title}".\n'
                     f"Эталонное наименование: {reference_name}.\n"
                     f"Проверьте соответствует ли ваше наименование '{name}'."
+                    f"<ins>Необходимо учесть требования постановления при проведении закупки.<ins>"
                 )
 
         return MatchResult(
